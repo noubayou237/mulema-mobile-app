c50579dc0fc347dfc628dd068c3ac01de17b429e
Warano02 <carineteoi@gmail.com>
2026-01-05 11:32:47 +0100

    remove nest

--- backend/src/learning/progress/progress.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProgressService } from './progress.service';
-import { ProgressController } from './progress.controller';
-
-@Module({
-  providers: [ProgressService],
-  controllers: [ProgressController]
-})
-export class ProgressModule {}
