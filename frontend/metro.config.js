fec735aa06adf5eca09b700aa9a4a183467c7d6d
leila237 <mbiydzenyuyeileen@gmail.com>
2026-04-27 17:18:46 +0100

    revert the image extension from png to webp

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,9 +3,6 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// ADD MOV to assetExts so Metro parses them correctly instead of treating them as JS/Babel code
-config.resolver.assetExts.push('mov', 'MOV');
-
 module.exports = withNativeWind(config, {
   input: "./global.css"
 });
